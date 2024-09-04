50e2336270e4466b3960e1092eba085e6bf4715a
InderGoswami <134797829+InderGoswami@users.noreply.github.com>
2024-09-04 17:37:26 +0530

    Delete Basics/Static Library directory

--- Basics/Static Library/addition.h
+++
@@ -1,3 +0,0 @@
-int add(int a,int b);
-//Only protype is mentioned here
-//This will allow user files to know about function without knowning its actual implementation
--- Basics/Static Library/source_file.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-#include"addition.h"
-int main(){
-    printf("%d",add(1,6));
-    return 0;
-}
